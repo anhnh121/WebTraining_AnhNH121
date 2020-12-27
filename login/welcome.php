5d7cc21d5c7cb66a62f98218a9c126688c72c19e
Nguyen Hai Anh <48093076+anhnh121@users.noreply.github.com>
2020-12-27 18:35:35 +0700

    CSS WELCOME

--- login/welcome.php
+++ login/welcome.php
@@ -29,11 +29,48 @@
 <html>
    <head>
       <title>Welcome </title>
+      <link rel="stylesheet" type="text/css" href="../topnav.css" />
+      <link rel="stylesheet" type="text/css" href="../tab.css" />
    </head>
    
    <body>
-      <h1>Welcome <?php echo $role . ": ". $login_name; ?></h1> 
-      <h2><a href = "logout.php">Sign Out</a></h2>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <div class="topnav-right">
+                <a style="color: crimson"><?php echo $role . ": ". $login_name; ?></a>
+                <a href = "logout.php">Sign Out</a>
+            </div>
+        </div>
+<!--        <div class="tab">
+            <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">London</button>
+            <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+            <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+        </div>
+        <script>
+            function openCity(evt, cityName) {
+              var i, tabcontent, tablinks;
+              tabcontent = document.getElementsByClassName("tabcontent");
+              for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+              }
+              tablinks = document.getElementsByClassName("tablinks");
+              for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+              }
+              document.getElementById(cityName).style.display = "block";
+              evt.currentTarget.className += " active";
+            }
+
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
+        </script>-->
+        <div class="tab">
+            <a class="active" href="#london">London</a>
+            <a href="#paris">Paris</a>
+            <a href="#hanoi">HaNoi</a>
+        </div>
    </body>
    
 </html>
